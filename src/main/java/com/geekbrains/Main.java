afba35121b3bfdd9da197a8adad5adf3245bed87
roman <romanfree1311@gmail.com>
2022-03-31 19:14:56 +0300

    lesson 2 java 2

--- src/main/java/com/geekbrains/Main.java
+++ src/main/java/com/geekbrains/Main.java
@@ -1,5 +1,38 @@
 package com.geekbrains;
 
 public class Main {
+    public static void main(String[] args) {
+        String[][] arr = new String[][]{{"1","2","3","4"}, {"2","1","3","1"}, {"1","3","4","2"}, {"1","1","1","1"}};
+        try {
+            try {
+                int result = method(arr);
+                System.out.println(result);
+            } catch (MyArraySizeException e){
+                System.out.println("Размер массива превышен");
+            }
+        } catch (MyArrayDataException e){
+            System.out.println("Неправильное значение массива");
+            System.out.println("Ошибка в ячейке:" + e.i + "x" + e.j);
+        }
+    }
 
+    public static int method(String[][] arr) throws MyArraySizeException, MyArrayDataException{
+        int count = 0;
+        if(arr.length != 4){
+            throw new MyArraySizeException();
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(arr.length != 4){
+                throw new MyArraySizeException();
+            }
+            for(int j = 0; j < arr.length; j++){
+                try{
+                    count = count + Integer.parseInt(arr[i][j]);
+                }catch (NumberFormatException e){
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return count;
+    }
 }
--- src/main/java/com/geekbrains/MyArrayDataException.java
+++ src/main/java/com/geekbrains/MyArrayDataException.java
@@ -1,4 +1,11 @@
 package com.geekbrains;
 
-public class MyArrayDataException {
+public class MyArrayDataException extends Exception{
+    public int i;
+    public int j;
+
+    public MyArrayDataException(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
 }
